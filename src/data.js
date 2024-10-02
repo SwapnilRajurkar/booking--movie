70fea18d59bccc926e684378bbe094c6ead177e3
SwapnilRajurkar <swapnilrajurkar12@gmail.com>
2024-10-02 22:45:50 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export let moviesList = ['Dunki', 'Stree-2', 'End Game', 'Jawan', 'Munja', "Navra Maza Navsacha-2", "Dharmaveer 2"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+
+// exports.moviesList = moviesList;
+// exports.slots = slots;
+// exports.seats = seats;
